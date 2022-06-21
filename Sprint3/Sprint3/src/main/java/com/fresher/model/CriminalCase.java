cd75422d2977f57d218e7c4fe0376811c521a57e
tuandv1999 <duongtuanyasua@gmail.com>
2022-06-21 08:36:20 +0700

    done

--- Sprint3/Sprint3/src/main/java/com/fresher/model/CriminalCase.java
+++ Sprint3/Sprint3/src/main/java/com/fresher/model/CriminalCase.java
@@ -3,25 +3,54 @@ package com.fresher.model;
 import com.fresher.core.CaseStatus;
 import com.fresher.core.CaseType;
 
-import java.util.HashSet;
-import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
 
-public class CriminalCase extends  AbstractEntity{
+@Entity
+@Table(name="criminalcase")
+public class CriminalCase {
+    @Id
+    @Column(name="id")
+    private Long id;
+    @Column(name="version")
+    private int  version;
+    @Column(name="number")
     private String number;
+    @Column(name="type")
     private CaseType type;
+    @Column(name="shortDescription")
     private String shortDescription;
+    @Column(name="detailedDescription")
     private String detailedDescription;
+    @Column(name="status")
     private CaseStatus status;
     //very big text
+    @Column(name="notes")
     private String notes;
-    private Set<Evidence> evidenceSet = new HashSet<>();
-    private Detective leadInvestigator;
-    private Set<Detective> assigned = new HashSet<>();
 
     public CriminalCase() {
         super();
         this.status = CaseStatus.SUBMITTED;
         this.type = CaseType.UNCATEGORIZED;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
     }
 
     public String getNumber() {
@@ -70,40 +99,40 @@ public class CriminalCase extends  AbstractEntity{
 
     public void setNotes(String notes) {
         this.notes = notes;
+
     }
 
-    public Set<Evidence> getEvidenceSet() {
-        return evidenceSet;
+    @Override
+    public String toString() {
+        return "CriminalCase{" +
+                "id=" + id +
+                ", version=" + version +
+                ", number='" + number + '\'' +
+                ", type=" + type +
+                ", shortDescription='" + shortDescription + '\'' +
+                ", detailedDescription='" + detailedDescription + '\'' +
+                ", status=" + status +
+                ", notes='" + notes + '\'' +
+                '}';
     }
 
-    public void setEvidenceSet(Set<Evidence> evidenceSetArg) {
-        evidenceSetArg.forEach(this :: addEvidence);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CriminalCase that = (CriminalCase) o;
+        return version == that.version &&
+                id.equals(that.id) &&
+                number.equals(that.number) &&
+                type == that.type &&
+                shortDescription.equals(that.shortDescription) &&
+                detailedDescription.equals(that.detailedDescription) &&
+                status == that.status &&
+                notes.equals(that.notes);
     }
 
-    public boolean addEvidence(Evidence evidence) {
-        evidence.setCriminalCase(this);
-        return evidenceSet.add(evidence);
-    }
-
-    public Detective getLeadInvestigator() {
-        return leadInvestigator;
-    }
-
-    public void setLeadInvestigator(Detective leadInvestigator) {
-        this.leadInvestigator = leadInvestigator;
-    }
-
-    public Set<Detective> getAssigned() {
-        return assigned;
-    }
-
-    public void setAssigned(Set<Detective> assignedArg) {
-        assignedArg.forEach(this:: addDetective);
-    }
-
-    //case is assigned to the detective, always use this method
-    public boolean addDetective(Detective detective) {
-        detective.addCase(this);
-        return assigned.add(detective);
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, version, number, type, shortDescription, detailedDescription, status, notes);
     }
 }
